95ddc3ca179e4056e4c199c895f3ee9d1a9ce171
HanatoK <summersnow9403@gmail.com>
2021-09-07 13:56:09 -0500

    Start to work on executor

--- Backend.cpp
+++ Backend.cpp
@@ -1,6 +1,8 @@
 #include "Backend.h"
+#include "IntermediateImpl.h"
 
 #include <chrono>
+#include <QCoreApplication>
 
 void Compiler::CodeGenerator::process(
     std::shared_ptr<ICode<ICodeNodeTypeImpl, ICodeKeyTypeImpl>> iCode,
@@ -14,10 +16,17 @@ void Compiler::CodeGenerator::process(
   summary(instructionCount, elapsed_time.count());
 }
 
+void Compiler::CodeGenerator::sendRuntimeErrorMessage(std::string msg, int line)
+{
+
+}
+
 void Interpreter::Executor::process(
     std::shared_ptr<ICode<ICodeNodeTypeImpl, ICodeKeyTypeImpl>> iCode,
     std::shared_ptr<SymbolTableStack<SymbolTableKeyTypeImpl>>
         symbol_table_stack) {
+  mICode = iCode;
+  mSymbolTableStack = symbol_table_stack;
   const auto start_time = std::chrono::high_resolution_clock::now();
   // do something
   const auto end_time = std::chrono::high_resolution_clock::now();
@@ -25,6 +34,11 @@ void Interpreter::Executor::process(
   const int executionCount = 0;
   const int runtimeErrors = 0;
   summary(executionCount, runtimeErrors, elapsed_time.count());
+}
+
+void Interpreter::Executor::sendRuntimeErrorMessage(std::string msg, int line)
+{
+
 }
 
 std::unique_ptr<Backend> createBackend(const std::string &operation) {
@@ -35,4 +49,44 @@ std::unique_ptr<Backend> createBackend(const std::string &operation) {
   } else {
     return nullptr;
   }
+}
+
+RuntimeErrorHandler::RuntimeErrorHandler(): mErrorCount(0)
+{
+
+}
+
+void RuntimeErrorHandler::flag(std::shared_ptr<ICodeNode<ICodeNodeTypeImpl, ICodeKeyTypeImpl> > node,
+                               RuntimeErrorCode error_code, std::shared_ptr<Backend> backend)
+{
+  const ICodeNodeBase* ptr = node.get();
+  while ((node != nullptr) && (!node->getAttribute(ICodeKeyTypeImpl::LINE).has_value())) {
+    ptr = node->parent();
+  }
+  const int line_number = std::any_cast<int>(ptr->getAttribute(ICodeKeyTypeImpl::LINE));
+  backend->sendRuntimeErrorMessage(runtimeErrorCodeToString(error_code), line_number);
+  if (++mErrorCount > MAX_ERRORS) {
+    std::cerr << "ABORTED AFTER TOO MANY RUNTIME ERRORS.\n";
+    QCoreApplication::exit(-1);
+  }
+}
+
+std::string runtimeErrorCodeToString(RuntimeErrorCode code)
+{
+  switch (code) {
+    case RuntimeErrorCode::DIVISION_BY_ZERO: {return "Division by zero";}
+    case RuntimeErrorCode::INVALID_CASE_EXPRESSION_VALUE: {return "Invalid CASE expression value";}
+    case RuntimeErrorCode::INVALID_INPUT: {return "Invalid input";}
+    case RuntimeErrorCode::INVALID_STANDARD_FUNCTION_ARGUMENT: {return "Invalid standard function argument";}
+    case RuntimeErrorCode::STACK_OVERFLOW: {return "Runtime stack overflow";}
+    case RuntimeErrorCode::UNIMPLEMENTED_FEATURE: {return "Unimplemented runtime feature";}
+    case RuntimeErrorCode::UNINITIALIZED_VALUE: {return "Uninitialized value";}
+    case RuntimeErrorCode::VALUE_RANGE: {return "Value out of range";}
+  }
+  return "Unknown runtime error.";
+}
+
+Backend::Backend()
+{
+
 }
--- Backend.h
+++ Backend.h
@@ -9,9 +9,34 @@
 #include <memory>
 #include <iostream>
 
+class Backend;
+
+enum class RuntimeErrorCode {
+  UNINITIALIZED_VALUE,
+  VALUE_RANGE,
+  INVALID_CASE_EXPRESSION_VALUE,
+  DIVISION_BY_ZERO,
+  INVALID_STANDARD_FUNCTION_ARGUMENT,
+  INVALID_INPUT,
+  STACK_OVERFLOW,
+  UNIMPLEMENTED_FEATURE
+};
+
+std::string runtimeErrorCodeToString(RuntimeErrorCode code);
+
+class RuntimeErrorHandler {
+public:
+  RuntimeErrorHandler();
+  void flag(std::shared_ptr<ICodeNode<ICodeNodeTypeImpl, ICodeKeyTypeImpl>> node,
+            RuntimeErrorCode error_code, std::shared_ptr<Backend> backend);
+private:
+  static const int MAX_ERRORS = 5;
+  int mErrorCount;
+};
+
 class Backend {
 public:
-  Backend() {}
+  Backend();
   virtual ~Backend() {
 #ifdef DEBUG_DESTRUCTOR
     std::cerr << "Destructor: " << BOOST_CURRENT_FUNCTION << std::endl;
@@ -22,9 +47,10 @@ public:
   virtual std::string getType() const {
     return "unknown";
   }
+  virtual void sendRuntimeErrorMessage(std::string msg, int line) = 0;
 protected:
   std::shared_ptr<ICode<ICodeNodeTypeImpl, ICodeKeyTypeImpl>> mICode;
-  std::shared_ptr<SymbolTable<SymbolTableKeyTypeImpl>> mSymbolTable;
+  std::shared_ptr<SymbolTableStack<SymbolTableKeyTypeImpl>> mSymbolTableStack;
 };
 
 namespace Compiler {
@@ -38,6 +64,7 @@ public:
   virtual std::string getType() const {
     return "compiler";
   }
+  virtual void sendRuntimeErrorMessage(std::string msg, int line);
   //  void summary(int instructionCount, float elapsedTime);
   boost::signals2::signal<void(int, float)> summary;
 };
@@ -55,6 +82,7 @@ public:
   virtual std::string getType() const {
     return "interpreter";
   }
+  virtual void sendRuntimeErrorMessage(std::string msg, int line);
   //  void summary(int executionCount, int runtimeErrors, float elapsedTime);
   boost::signals2::signal<void(int, int, float)> summary;
 };
