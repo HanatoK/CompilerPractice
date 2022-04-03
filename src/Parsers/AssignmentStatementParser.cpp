82985d1b509765a439508a599db3cb7195ad5ff2
HanatoK <summersnow9403@gmail.com>
2022-04-03 11:04:16 -0500

    Type checking in assignment statement parser

--- src/Parsers/AssignmentStatementParser.cpp
+++ src/Parsers/AssignmentStatementParser.cpp
@@ -1,6 +1,8 @@
 #include "AssignmentStatementParser.h"
 #include "ExpressionParser.h"
 #include "StatementParser.h"
+#include "VariableParser.h"
+#include "TypeChecker.h"
 
 AssignmentStatementParser::AssignmentStatementParser(const std::shared_ptr<PascalParserTopDown>& parent): PascalSubparserTopDownBase(parent)
 {
@@ -16,22 +18,16 @@ AssignmentStatementParser::~AssignmentStatementParser()
 
 std::shared_ptr<ICodeNodeImplBase> AssignmentStatementParser::parse(std::shared_ptr<PascalToken> token)
 {
+  // create the assign node
   auto assign_node = std::shared_ptr(createICodeNode(ICodeNodeTypeImpl::ASSIGN));
-  // lookup the target identifier in the symbol table stack
-  const std::string target_name = boost::algorithm::to_lower_copy(token->text());
-  auto target_id = getSymbolTableStack()->lookup(target_name);
-  // enter the identifier into the table if not found
-  if (target_id == nullptr) {
-    target_id = getSymbolTableStack()->enterLocal(target_name);
-  }
-  target_id->appendLineNumber(token->lineNum());
-  // create the variable node and set its name attribute
-  auto variable_node = createICodeNode(ICodeNodeTypeImpl::VARIABLE);
-  variable_node->setAttribute(ICodeKeyTypeImpl::ID, target_id);
-  // the assign node adopts the variable node as its child
-  assign_node->addChild(std::move(variable_node));
-  // consume the identifier token
-  nextToken();
+  // parse the target variable
+  VariableParser variable_parser(currentParser());
+  auto target_node = variable_parser.parse(token);
+  const auto target_type = (target_node != nullptr) ? target_node->getTypeSpec() :
+                                            Predefined::instance().undefinedType;
+  // the ASSIGN node adopts the variable node as its first child
+  assign_node->addChild(target_node);
+  // synchronize on the := token
   token = synchronize(AssignmentStatementParser::colonEqualsSet());
   // and look for the := token
   if (token->type() == PascalTokenTypeImpl::COLON_EQUALS) {
@@ -42,7 +38,16 @@ std::shared_ptr<ICodeNodeImplBase> AssignmentStatementParser::parse(std::shared_
   }
   // parse the expression
   ExpressionParser expression_parser(currentParser());
-  assign_node->addChild(expression_parser.parse(token));
+  auto expression_node = expression_parser.parse(token);
+  // type check: assignment compatible?
+  const auto expression_type = (expression_node != nullptr) ?
+                               expression_node->getTypeSpec() : Predefined::instance().undefinedType;
+  using namespace TypeChecker::TypeCompatibility;
+  if (!areAssignmentCompatible(target_type, expression_type)) {
+    errorHandler()->flag(token, PascalErrorCode::INCOMPATIBLE_TYPES, currentParser());
+  }
+  assign_node->addChild(std::move(expression_node));
+  assign_node->setTypeSpec(target_type);
   return assign_node;
 }
 
